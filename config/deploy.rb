cbd2154744d460db7a3708ad8e7088cb148bc542
yu-su8016 <63726642+yu-su8016@users.noreply.github.com>
2020-07-20 17:15:03 +0900

    unicorn修正2

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,6 @@ set :keep_releases, 5
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
+    invoke 'unicorn:restart'
   end
 end
