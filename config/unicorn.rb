9f68921c765688ef3477ca1fd1953951a833c71a
yu-su8016 <63726642+yu-su8016@users.noreply.github.com>
2020-07-22 09:37:22 +0900

    app記載追加

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,10 +3,10 @@ app_path = File.expand_path('../../../', __FILE__)
 worker_processes 1
 working_directory "#{app_path}/kirakushare/current"
 
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+listen "#{app_path}/kirakushare/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/kirakushare/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/kirakushare/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/kirakushare/shared/log/unicorn.stdout.log"
 
 timeout 60
 
