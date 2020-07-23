81dc26e17d953899b7f0b0e07811224d30730cb9
yu-su8016 <63726642+yu-su8016@users.noreply.github.com>
2020-07-23 10:28:06 +0900

    capistirano

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
-working_directory "#{app_path}/current"
+working_directory "app_path"
 
 listen "#{app_path}/tmp/sockets/unicorn.sock"
 pid "#{app_path}/tmp/pids/unicorn.pid"
